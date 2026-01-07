f58b72ceb890fcd737f519b3348a485aa4112b77
Bert Shuler <bshuler@proton.me>
2026-01-07 11:30:06 -0500

    Fix build.gradle.kts: use mod.isFabric, remove unsupported DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,14 @@
+import gg.meza.stonecraft.mod
+
 plugins {
     id("gg.meza.stonecraft")
-}
-
-modSettings {
-
-    // This is a client-side only mod
-    clientSideOnly = true
-
-    // Mixin configuration
-    mixins {
-        add("flight_display.mixins.json")
-    }
 }
 
 // Publishing configuration for CurseForge and Modrinth
 publishMods {
     modrinth {
         // Fabric requires Fabric API
-        if (isFabric) {
+        if (mod.isFabric) {
             requires("fabric-api")
         }
     }
@@ -25,7 +16,7 @@ publishMods {
         clientRequired = true
         serverRequired = false
         // Fabric requires Fabric API
-        if (isFabric) {
+        if (mod.isFabric) {
             requires("fabric-api")
         }
     }
